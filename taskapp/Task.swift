ae028fdff0de6a7cc09b296e0729f2506f458837
春山優太 <haruyamayuuta@haruyamayuutanomacbook-puro.local>
2018-08-07 18:04:55 +0900

    タスクアプリ

---
+++ taskapp/Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 春山優太 on 2018/07/23.
+//  Copyright © 2018年 haruyam. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+    
+    // タイトル
+    @objc dynamic var title = ""
+    
+    // 内容
+    @objc dynamic var contents = ""
+    
+    /// 日時
+    @objc dynamic var date = Date()
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
